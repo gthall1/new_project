5ffe11d3e285aee9dfe1e3029dcf6c00549ee767
Linux Desktop <tyler@ubuntu.(none)>
2015-02-22 18:13:46 -0500

    trying to get js to compile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,7 +19,7 @@ Alotto::Application.configure do
   # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
   # config.action_dispatch.rack_cache = true
 
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor  = :uglifier
@@ -58,8 +58,8 @@ Alotto::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  #config.assets.precompile += %w( game.js )
-  config.assets.precompile += ['game.js']
+  config.assets.precompile += %w( game.js )
+ # config.assets.precompile += ['game.js']
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
