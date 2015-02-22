46d78d932e1b81d7a7dcb372c50741870a197207
Linux Desktop <tyler@ubuntu.(none)>
2015-02-22 17:59:52 -0500

    some fixes

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ActiveRecord::Base
   belongs_to :jackpot
 
   before_create :create_remember_token
+
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
