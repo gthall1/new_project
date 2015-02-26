953526192554fd8c07211f50788fd52868f51d7d
Linux Desktop <tyler@ubuntu.(none)>
2015-02-25 22:15:05 -0500

    fix for memory game

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,8 @@ Alotto::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  config.assets.precompile += %w( game.js )
+  config.assets.precompile += %w( game.js custom.css)
+
  # config.assets.precompile += ['game.js']
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
