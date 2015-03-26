c23feeda487dc6bdf6b4677a7742b5f68ed32850
Linux Desktop <tyler@ubuntu.(none)>
2015-03-25 21:20:11 -0400

    fixing stuff

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -10,9 +10,8 @@ class GamesController < ApplicationController
   # GET /games/1
   # GET /games/1.json
   def show
-    @game = Game.find(params[:id])
-    init_testgame(params[:id]) unless @game.name == "Memory Game"
-    @current_high_score = UserGameSession.where(user_id:current_user.id,game_id:params[:id]).where.not(score: nil).order("score desc").first
+    init_testgame(@game.id) unless @game.name == "Memory Game"
+    @current_high_score = UserGameSession.where(user_id:current_user.id,game_id:@game.id).where.not(score: nil).order("score desc").first
     @current_high_score = Time.at(@current_high_score.score).utc.strftime("%M:%S") if @current_high_score
   end
   
