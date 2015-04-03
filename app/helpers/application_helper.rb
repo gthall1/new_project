0832c802669156c2656536b80851d299115e46b4
Griff Hall <griffhall1@gmail.com>
2015-04-03 18:09:04 -0500

    fixed sublogo bug

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   # Returns the full title on a per-page basis.
   def full_title(page_title)
-    base_title = "@lotto"
+    base_title = "Luckee"
     if page_title.empty?
       base_title
     else
