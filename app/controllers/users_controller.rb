736a5a9b74b8a4be381f328a94e952de14f98487
Griff Hall <griffhall1@gmail.com>
2015-04-04 16:16:42 -0500

    signup routes changes to root instead of profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,7 +21,7 @@ class UsersController < ApplicationController
     if @user.save
       sign_in @user
       flash[:success] = "Welcome to Luckee!"
-      redirect_to @user
+      redirect_to(root_url)
     else
       render 'new'
     end
