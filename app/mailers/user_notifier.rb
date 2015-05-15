9fd50d45be6570bd91334f514056798d4ab8dbda
teylewr <tylerrules@gmail.com>
2015-05-14 22:49:06 -0400

    updating some of the styles on email

--- app/mailers/user_notifier.rb
+++ app/mailers/user_notifier.rb
@@ -1,12 +1,15 @@
 class UserNotifier < ActionMailer::Base
-  default from: "gamemaster@getluckee.com"
+  default from: "GetLuckee <gamemaster@getluckee.com>"
   layout 'mailer'
 
   def send_credit_reminder(args)
-  	user = User.where(id:args[:user_id]).first
-  	@subject = "Time is running out!"
-  	@short_description = "Time is running out! Deposit your credits before it is too late!"
-    mail( :to => user.email,
-    :subject => "Don't miss out on great prizes!" )
+  	@user = User.where(id:args[:user_id]).first
+
+  	if @user.credits > 0
+  		@head_message = "You have #{@user.credits} credits ready for deposit! Make sure to deposit all of your credits to increase your chances of winning the grand prize of #{Jackpot.where(open:true).last.prize}!"
+	    mail( :to => @user.email,
+	    :reply_to => 'griffhall1@gmail.com',
+	    :subject => "Don't miss out on great prizes!" )
+	end
   end
 end
