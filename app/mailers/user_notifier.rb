bf147412b2f47c32e4f09de945a407a67cda318a
teylewr <tylerrules@gmail.com>
2015-05-14 23:00:03 -0400

    fixing text

--- app/mailers/user_notifier.rb
+++ app/mailers/user_notifier.rb
@@ -9,6 +9,7 @@ class UserNotifier < ActionMailer::Base
   		@head_message = "You have #{@user.credits} credits ready for deposit! Make sure to deposit all of your credits to increase your chances of winning the grand prize of #{Jackpot.where(open:true).last.prize}!"
 	    mail( :to => @user.email,
 	    :reply_to => 'griffhall1@gmail.com',
+	    :display_name => "GetLuckee",
 	    :subject => "Don't miss out on great prizes!" )
 	end
   end
