7d81de4ce8a02702edb0ed2d308a0744578ddb05
AlexMorgan <alexmorgan.am@gmail.com>
2015-10-31 09:56:37 -0400

    cleaned up redeem credits page

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -3,25 +3,27 @@ $(document).ready(function(){
 
   app.init();
   $( ".paypal-choice" ).click(function() {
-	 $(".paypal-choice").hide();
-	 $(".venmo-choice").hide();
-	 $(".paypal-select").show();
-	 $(".final-step").show();
-	 $("#cash_out_cashout_type").val(1);
+    $('.page-title').html('Paypal Details');
+    $(".paypal-choice").hide();
+    $(".venmo-choice").hide();
+    $(".paypal-select").show();
+    $(".final-step").show();
+    $("#cash_out_cashout_type").val(1);
    });
   $( ".venmo-choice" ).click(function() {
-	 $(".venmo-choice").hide();
-	 $(".paypal-choice").hide();
-	 $(".venmo-select").show()
-	 $(".final-step").show();
-	 $("#cash_out_cashout_type").val(0);
-   });  
+      $('.page-title').html('Venmo Details');
+      $(".venmo-choice").hide();
+      $(".paypal-choice").hide();
+      $(".venmo-select").show()
+      $(".final-step").show();
+      $("#cash_out_cashout_type").val(0);
+   });
   $( ".back-choice" ).click(function() {
-	 $(".venmo-choice").show();
-	 $(".paypal-choice").show();
-	 $(".venmo-select").hide();
-	 $(".paypal-select").hide();
-	 $(".final-step").hide();
-
+    $('.page-title').html('Cash Type');
+    $(".venmo-choice").show();
+    $(".paypal-choice").show();
+    $(".venmo-select").hide();
+    $(".paypal-select").hide();
+    $(".final-step").hide();
    });
 });
