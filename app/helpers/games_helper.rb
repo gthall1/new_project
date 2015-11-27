55dcfee90fc96d593201a93f612f3bbcab3c5156
teylewr <tylerrules@gmail.com>
2015-11-27 01:48:54 -0500

    fixing issue from deprecated function in rails update for leaderboard

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -2,24 +2,45 @@ module GamesHelper
 	def get_daily_leaderboard(args={})
 		game_id = args[:game_id]
 		game = Game.where(id:game_id).first
+		users = []
+		scores = []		
 		if game
-			UserGameSession.where(game_id:game.id).where.not(score:nil,score:0).where("created_at >= ?",Time.now.beginning_of_day).maximum(:score, :group=>'user_id').sort_by{|k,v| v}.reverse[0..9].map{ |a| [User.where(id:a[0]).first.present? ? User.find(a[0]).name : "Deleted" ,a[1]]}			
-		end			
+			UserGameSession.where(game_id:game.id).where("created_at >= ?",Time.now.beginning_of_day).where.not(score:nil,score:0).order("score desc").each do | u |
+				next if users.include?(u.user_id)
+				users << u.user_id
+				scores << [User.where(id:u.user_id).first.present? ? User.find(u.user_id).name : "Deleted" ,u.score]
+			end		
+		end	
+		scores[0..9]				
 	end
 
 	def get_weekly_leaderboard(args={})
 		game_id = args[:game_id]
 		game = Game.where(id:game_id).first
+		users = []
+		scores = []		
 		if game
-			UserGameSession.where(game_id:game.id).where.not(score:nil,score:0).where("created_at >= ?",Time.now.beginning_of_week).maximum(:score, :group=>'user_id').sort_by{|k,v| v}.reverse[0..9].map{ |a| [User.where(id:a[0]).first.present? ? User.find(a[0]).name : "Deleted" ,a[1]]}			
-		end		
+			UserGameSession.where(game_id:game.id).where("created_at >= ?",Time.now.beginning_of_week).where.not(score:nil,score:0).order("score desc").each do | u |
+				next if users.include?(u.user_id)
+				users << u.user_id
+				scores << [User.where(id:u.user_id).first.present? ? User.find(u.user_id).name : "Deleted" ,u.score]
+			end		
+		end	
+		scores[0..9]
 	end	
 
 	def get_alltime_leaderboard(args={})
 		game_id = args[:game_id]
+		users = []
+		scores = []
 		game = Game.where(id:game_id).first
 		if game
-			UserGameSession.where(game_id:game.id).where.not(score:nil,score:0).maximum(:score, :group=>'user_id').sort_by{|k,v| v}.reverse[0..9].map{ |a| [User.where(id:a[0]).first.present? ? User.find(a[0]).name : "Deleted" ,a[1]]}			
+			UserGameSession.where(game_id:game.id).where.not(score:nil,score:0).order("score desc").each do | u |
+				next if users.include?(u.user_id)
+				users << u.user_id
+				scores << [User.where(id:u.user_id).first.present? ? User.find(u.user_id).name : "Deleted" ,u.score]
+			end
 		end
+		scores[0..9]
 	end	
 end
