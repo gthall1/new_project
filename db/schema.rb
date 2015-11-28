139a20ffb41f84bde5a8cf41d853e3cf3e0c27bb
AlexMorgan <alexmorgan.am@gmail.com>
2015-11-27 22:43:47 -0500

    removed default notification alert and default avatar in nav

--- db/schema.rb
+++ db/schema.rb
@@ -11,114 +11,125 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151104041029) do
+ActiveRecord::Schema.define(version: 20151127221259) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "advertisers", force: true do |t|
-    t.string   "name"
-    t.string   "logo"
+  create_table "advertisers", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.string   "logo",       limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "arrivals", force: true do |t|
-    t.string   "landing_url"
-    t.string   "referer"
-    t.string   "user_agent"
-    t.string   "ip"
-    t.string   "mobile"
+  create_table "arrivals", force: :cascade do |t|
+    t.string   "landing_url", limit: 255
+    t.string   "referer",     limit: 255
+    t.string   "user_agent",  limit: 255
+    t.string   "ip",          limit: 255
+    t.string   "mobile",      limit: 255
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "cash_outs", force: true do |t|
+  create_table "cash_outs", force: :cascade do |t|
     t.integer  "user_id"
     t.integer  "credits"
     t.integer  "cash"
-    t.string   "venmo"
-    t.string   "paypal"
+    t.string   "venmo",        limit: 255
+    t.string   "paypal",       limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "cashout_type"
     t.integer  "arrival_id"
   end
 
-  create_table "feed_games", force: true do |t|
-    t.string   "name"
-    t.string   "description"
-    t.string   "thumb_60"
-    t.string   "thumb_120"
-    t.string   "thumb_180"
-    t.string   "external_link"
-    t.string   "orientation"
+  create_table "feed_games", force: :cascade do |t|
+    t.string   "name",          limit: 255
+    t.string   "description",   limit: 255
+    t.string   "thumb_60",      limit: 255
+    t.string   "thumb_120",     limit: 255
+    t.string   "thumb_180",     limit: 255
+    t.string   "external_link", limit: 255
+    t.string   "orientation",   limit: 255
     t.datetime "game_added"
     t.decimal  "aspect_ratio"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "game_categories", force: true do |t|
-    t.string   "name"
+  create_table "game_categories", force: :cascade do |t|
+    t.string   "name",       limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "game_category_rels", force: true do |t|
+  create_table "game_category_rels", force: :cascade do |t|
     t.integer  "feed_game_id"
     t.integer  "category_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "games", force: true do |t|
-    t.string   "name"
+  create_table "games", force: :cascade do |t|
+    t.string   "name",        limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "image"
+    t.string   "image",       limit: 255
     t.integer  "device_type"
-    t.string   "slug"
+    t.string   "slug",        limit: 255
+    t.integer  "sort_order"
   end
 
-  create_table "identities", force: true do |t|
+  create_table "identities", force: :cascade do |t|
     t.integer  "user_id"
-    t.string   "provider"
-    t.string   "uid"
+    t.string   "provider",   limit: 255
+    t.string   "uid",        limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   add_index "identities", ["user_id"], name: "index_identities_on_user_id", using: :btree
 
-  create_table "jackpots", force: true do |t|
-    t.string   "name"
+  create_table "jackpots", force: :cascade do |t|
+    t.string   "name",        limit: 255
     t.integer  "max_entries"
     t.datetime "draw_date"
-    t.string   "prize"
+    t.string   "prize",       limit: 255
     t.boolean  "open"
     t.integer  "winner_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "surveys", force: true do |t|
+  create_table "sessions", force: :cascade do |t|
+    t.string   "session_id", null: false
+    t.text     "data"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "sessions", ["session_id"], name: "index_sessions_on_session_id", unique: true, using: :btree
+  add_index "sessions", ["updated_at"], name: "index_sessions_on_updated_at", using: :btree
+
+  create_table "surveys", force: :cascade do |t|
     t.integer  "credits"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "name"
+    t.string   "name",       limit: 255
   end
 
-  create_table "user_entries", force: true do |t|
+  create_table "user_entries", force: :cascade do |t|
     t.integer  "user_id"
     t.integer  "jackpot_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "user_game_sessions", force: true do |t|
-    t.string   "token"
+  create_table "user_game_sessions", force: :cascade do |t|
+    t.string   "token",           limit: 255
     t.integer  "user_id"
     t.integer  "game_id"
     t.integer  "credits_earned"
@@ -127,11 +138,11 @@ ActiveRecord::Schema.define(version: 20151104041029) do
     t.datetime "updated_at"
     t.integer  "score"
     t.integer  "challenge_id"
-    t.integer  "credits_applied", default: 0
+    t.integer  "credits_applied",             default: 0
     t.integer  "arrival_id"
   end
 
-  create_table "user_surveys", force: true do |t|
+  create_table "user_surveys", force: :cascade do |t|
     t.integer  "user_id"
     t.integer  "survey_id"
     t.boolean  "complete"
@@ -140,33 +151,33 @@ ActiveRecord::Schema.define(version: 20151104041029) do
     t.integer  "arrival_id"
   end
 
-  create_table "users", force: true do |t|
-    t.string   "name"
-    t.string   "email"
+  create_table "users", force: :cascade do |t|
+    t.string   "name",             limit: 255
+    t.string   "email",            limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "password_digest"
-    t.string   "remember_token"
+    t.string   "password_digest",  limit: 255
+    t.string   "remember_token",   limit: 255
     t.boolean  "admin"
     t.integer  "credits"
     t.date     "dob"
-    t.string   "zipcode"
+    t.string   "zipcode",          limit: 255
     t.integer  "gender"
-    t.string   "provider"
-    t.string   "uid"
-    t.string   "oath_name"
-    t.string   "oath_image"
-    t.string   "oath_token"
+    t.string   "provider",         limit: 255
+    t.string   "uid",              limit: 255
+    t.string   "oath_name",        limit: 255
+    t.string   "oath_image",       limit: 255
+    t.string   "oath_token",       limit: 255
     t.datetime "oath_expires_at"
     t.float    "latitude"
     t.float    "longitude"
     t.integer  "lifetime_credits"
     t.integer  "pending_credits"
-    t.string   "referral"
-    t.string   "reset_digest"
+    t.string   "referral",         limit: 255
+    t.string   "reset_digest",     limit: 255
     t.datetime "reset_sent_at"
-    t.string   "firstname"
-    t.string   "lastname"
+    t.string   "firstname",        limit: 255
+    t.string   "lastname",         limit: 255
     t.integer  "arrival_id"
   end
 
