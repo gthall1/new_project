6c6434e9a8ed6db2dff5f085da99f8b442ad75e8
teylewr <tylerrules@gmail.com>
2015-12-27 18:07:33 -0500

    fixing migration

--- db/migrate/20151220182634_add_scores_to_challenges.rb
+++ db/migrate/20151220182634_add_scores_to_challenges.rb
@@ -1,6 +1,6 @@
 class AddScoresToChallenges < ActiveRecord::Migration
   def change
     add_column :challenges, :user_score, :integer
-    add_column :challenges, :challenged_score, :integer
+    #add_column :challenges, :challenged_score, :integer
   end
 end
