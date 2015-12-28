43c2b13af2f6ac3f361c60488cd30a93173e318e
teylewr <tylerrules@gmail.com>
2015-12-27 19:00:20 -0500

    fixing Facebook challenges

--- app/assets/javascripts/challenge.js
+++ app/assets/javascripts/challenge.js
@@ -1,10 +1,18 @@
 $(document).ready(function(){
 	$( ".random-user-but" ).click(function() {
-		console.log('ruh-row');
 		$.ajax({
 		  type: "GET",
 		  url: "/ro"
 		})
 	});
 	
+	$(".challenged-user").click(function(){
+		$('#challenge_challenged_user_id').val($(this).data("user-id"));
+		$('#challenge_challenged_user_id').val($(this).data("user-id"));
+		$('.challenged-user-name').html($(this).data("user-name"));
+		$('.pre-challenge').show();
+		$('.challenge-choice').hide();		
+	});
+
 });
+
