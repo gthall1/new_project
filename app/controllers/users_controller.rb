1d0b42b894246a174efeae5f9d39b955cb37a1d9
teylewr <tylerrules@gmail.com>
2016-01-24 17:11:45 -0500

    fixing to remove device type nil

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,7 +21,7 @@ class UsersController < ApplicationController
 
     def show
         @current_page = "profile"
-        all_games = Game.all.order(:slug)
+        all_games = Game.where.not(devie_type:nil).order(:slug)
 
 
         # Set up json arr
