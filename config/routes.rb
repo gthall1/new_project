eb93fa0ff6bd6313549155576080f885a70e5983
teylewr <tylerrules@gmail.com>
2016-02-01 22:30:15 -0500

    adding api things for pulling data

--- config/routes.rb
+++ config/routes.rb
@@ -1,63 +1,71 @@
 Alotto::Application.routes.draw do
-  resources :surveys
+    resources :surveys
 
-  resources :feed_games
+    resources :games
 
-  resources :jackpots
+    resources :users
+    resources :sessions,      only: [:new, :create, :destroy]
+    resources :password_resets,     only: [:new, :create, :edit, :update]
+    resources :cash_outs, only: [:create]
+    root to: 'static_pages#home'
 
-  resources :games
+    match '/signup',  to: 'users#new',            via: 'get'
+    match '/signin',  to: 'sessions#new',         via: 'get'
+    match '/signout', to: 'sessions#destroy',     via: 'delete'
+    match '/faq',    to: 'static_pages#faq',    via: 'get'
+    match '/onboarding', to: 'static_pages#onboarding', via: 'get'
+    match '/how_it_works', to: 'static_pages#how_it_works', via: 'get'
+    match '/about',   to: 'static_pages#about',   via: 'get'
+    match '/redeem',   to: 'static_pages#redeem',   via: 'get'
+    match '/donate',   to: 'static_pages#donate',   via: 'get'
+    match '/redeem_credits/(:credits)',   to: 'static_pages#redeem_credits',as: 'redeem_credits',  via: 'get'
+    match '/donate_credits/(:credits)',   to: 'static_pages#donate_credits',as: 'donate_credits',  via: 'get'
+    match '/refer',   to: 'static_pages#refer',   via: 'get'
+    match '/contact', to: 'static_pages#contact', via: 'get'
+    match '/2048_tutorial', to: 'static_pages#twentyfortyeight_tut', via: 'get'
+    match '2048_home', to: 'games#twentyfortyeight_home', via: 'get'
+    match '/updates', to: 'games#test_game_check', via: 'get'
+    match '/user_survey', to: 'surveys#user_survey_save', via: 'patch'
+    match '/heliwin', to: 'games#helicopter_check', via: 'get'
+    match '/sorcend', to: 'games#sorcerer_end', via: 'get'
+    match '/score_update', to: 'games#score_update', via: 'get'
+    match '/score_update', to: 'games#score_update', via: 'post'
+    match '/get_high_scores', to: 'games#get_high_scores', via: 'get'
+    match '/close_game', to: 'games#close_game', via: 'post'
+    match '/close_game', to: 'games#close_game', via: 'get'
+    match '/new_game', to: 'games#new_game_session', via: 'get'
+    match '/reset_game', to: 'games#reset_game', via: 'get'
+    match '/reset_game', to: 'games#reset_game', via: 'post'
+    match '/memorywin', to: 'games#memory_check', via: 'get'
+    match '/challenge/:game_slug', to: 'games#challenge', as: 'game_challenge', via: 'get'
+    match '/challenge_play/:c', to: 'games#show', as: 'challenge_accept', via: 'get'
+    match '/challenge_user', to: 'games#challenge_create',as: 'challenges', via: 'post'
+    match '/ro', to: 'games#get_random_challenge_user', via: 'get'
+    match '/leaderboard/:game_slug', to: 'games#leaderboard', as: 'game_leaderboard', via: 'get'
+    match '/leaderboards', to: 'games#games_leaderboard', via: 'get'
+    match '/cash_out', to: 'static_pages#new_cash_out', via: 'post'
+    match '/donation', to: 'static_pages#new_donation', via: 'post'
+    match '/get_advertisers', to: 'games#get_advertisers', via: 'get'
+    match '/reset_timer', to: 'games#reset_game', via: 'get'
+    match '/get_advertiser_logo', to: 'games#get_advertiser_logo', via: 'get'
+    match '/current_jackpot', to:'jackpots#current_jackpot', via: 'get'
+    match '/deposit',  to: 'jackpots#show',         via: 'get'
+    match '/stats', to: 'users#stats', via: 'get'
+    match '/challenges', to: 'users#challenges',as: 'user_challenges', via: 'get'
+    match '/invite/:referral',  to: 'static_pages#home_invite',         via: 'get'
+    get 'auth/:provider/callback', to: 'sessions#create_from_facebook'
 
-  resources :users
-  resources :sessions,      only: [:new, :create, :destroy]
-  resources :microposts,    only: [:create, :destroy]
-  resources :relationships, only: [:create, :destroy]
-  resources :password_resets,     only: [:new, :create, :edit, :update]
-  resources :cash_outs, only: [:create]
-  root to: 'static_pages#home'
-  match '/signup',  to: 'users#new',            via: 'get'
-  match '/signin',  to: 'sessions#new',         via: 'get'
-  match '/signout', to: 'sessions#destroy',     via: 'delete'
-  match '/faq',    to: 'static_pages#faq',    via: 'get'
-  match '/onboarding', to: 'static_pages#onboarding', via: 'get'
-  match '/how_it_works', to: 'static_pages#how_it_works', via: 'get'
-  match '/about',   to: 'static_pages#about',   via: 'get'
-  match '/redeem',   to: 'static_pages#redeem',   via: 'get'
-  match '/donate',   to: 'static_pages#donate',   via: 'get'
-  match '/redeem_credits/(:credits)',   to: 'static_pages#redeem_credits',as: 'redeem_credits',  via: 'get'
-  match '/donate_credits/(:credits)',   to: 'static_pages#donate_credits',as: 'donate_credits',  via: 'get'
-  match '/refer',   to: 'static_pages#refer',   via: 'get'
-  match '/contact', to: 'static_pages#contact', via: 'get'
-  match '/2048_tutorial', to: 'static_pages#twentyfortyeight_tut', via: 'get'
-  match '2048_home', to: 'games#twentyfortyeight_home', via: 'get'
-  match '/updates', to: 'games#test_game_check', via: 'get'
-  match '/user_survey', to: 'surveys#user_survey_save', via: 'patch'
-  match '/heliwin', to: 'games#helicopter_check', via: 'get'
-  match '/sorcend', to: 'games#sorcerer_end', via: 'get'
-  match '/score_update', to: 'games#score_update', via: 'get'
-  match '/score_update', to: 'games#score_update', via: 'post'
-  match '/get_high_scores', to: 'games#get_high_scores', via: 'get'
-  match '/close_game', to: 'games#close_game', via: 'post'
-  match '/close_game', to: 'games#close_game', via: 'get'
-  match '/new_game', to: 'games#new_game_session', via: 'get'
-  match '/reset_game', to: 'games#reset_game', via: 'get'
-  match '/reset_game', to: 'games#reset_game', via: 'post'
-  match '/memorywin', to: 'games#memory_check', via: 'get'
-  match '/challenge/:game_slug', to: 'games#challenge', as: 'game_challenge', via: 'get'
-  match '/challenge_play/:c', to: 'games#show', as: 'challenge_accept', via: 'get'
-  match '/challenge_user', to: 'games#challenge_create',as: 'challenges', via: 'post'
-  match '/ro', to: 'games#get_random_challenge_user', via: 'get'
-  match '/leaderboard/:game_slug', to: 'games#leaderboard', as: 'game_leaderboard', via: 'get'
-  match '/leaderboards', to: 'games#games_leaderboard', via: 'get'
-  match '/cash_out', to: 'static_pages#new_cash_out', via: 'post'
-  match '/donation', to: 'static_pages#new_donation', via: 'post'
-  match '/get_advertisers', to: 'games#get_advertisers', via: 'get'
-  match '/reset_timer', to: 'games#reset_game', via: 'get'
-  match '/get_advertiser_logo', to: 'games#get_advertiser_logo', via: 'get'
-  match '/current_jackpot', to:'jackpots#current_jackpot', via: 'get'
-  match '/deposit',  to: 'jackpots#show',         via: 'get'
-  match '/stats', to: 'users#stats', via: 'get'
-  match '/challenges', to: 'users#challenges',as: 'user_challenges', via: 'get'
-  match '/invite/:referral',  to: 'static_pages#home_invite',         via: 'get'
-  get 'auth/:provider/callback', to: 'sessions#create_from_facebook'
-
+    #For analytics to grab some data
+    namespace :api, defaults: { format: :json } do 
+        namespace :v1 do
+            scope ':token' do
+                match '/users', to: 'users#get_users', via: 'get'
+                match '/user_games', to: 'users#get_game_sessions', via: 'get'
+                match '/games', to: 'games#get_games', via: 'get'
+                match '/surveys', to: 'games#get_surveys', via: 'get'
+                match '/cashouts', to: 'users#get_cash_outs', via: 'get'
+            end
+        end
+    end
+    
 end
---
+++ db/migrate/20160201040242_create_api_users.rb
@@ -0,0 +1,10 @@
+class CreateApiUsers < ActiveRecord::Migration
+  def change
+    create_table :api_users do |t|
+      t.string :token
+      t.integer :user_id
+
+      t.timestamps null: false
+    end
+  end
+end
