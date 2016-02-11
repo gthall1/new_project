3d49be66be81c86d8e25752fb9f7906c5f100b29
teylewr <tylerrules@gmail.com>
2016-02-10 20:31:57 -0500

    adding last_score_update to game session for more accurate timing

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -159,7 +159,8 @@ class GamesController < ApplicationController
 
          # p "Score : #{score} | Game SEssion Score: #{game_session.score}"
             if game_session && game_session.active
-                 credits_to_apply = get_credits_to_apply(game_session.game.slug,score,game_session.credits_applied,game_session.version)
+                game_session.last_score_update = Time.now
+                credits_to_apply = get_credits_to_apply(game_session.game.slug,score,game_session.credits_applied,game_session.version)
                 if user && credits_to_apply > 0
                     user.add_credits({credits:credits_to_apply})
                     game_session.credits_applied += credits_to_apply
