f36d301f20ef8aa4c05777adc6a79d5a146a817b
teylewr <tylerrules@gmail.com>
2016-03-01 11:42:21 -0500

    updating friend leaderboard, to be a leaderboard of friends

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -36,6 +36,50 @@ module GamesHelper
         scores[0..9]
     end 
 
+
+
+    def get_friend_leaderboard(args={})
+        game_id = args[:game_id]
+        version = args[:version] ||= nil
+        game = Game.where(id:game_id).first
+        users = []
+        scores = []     
+        if game && current_user
+            own_best_session = UserGameSession.where(game_id:game_id,version:version,user_id:current_user.id).order('score desc').first
+            if own_best_session.blank?
+                own_best_score = 0
+            else
+                own_best_score = own_best_session.score
+            end
+            scores << [current_user.name,current_user.oath_image,own_best_score]
+            if session[:auth_token] 
+                graph = Koala::Facebook::API.new(session[:auth_token]) 
+                friends = graph.get_connections("me", "friends")  
+                if !friends.blank? 
+                    friends.each do | f | 
+                        friend = User.where(uid:f["id"]).first 
+                        if !friend.blank?
+                            friend_best_session = UserGameSession.where(game_id:game_id,version:version,user_id:friend.id).order('score desc').first
+                            if friend_best_session.blank?
+                                friend_score = 0
+                            else
+                                friend_score = friend_best_session.score
+                            end
+                            scores << [friend.name,friend.oath_image,friend_score]
+                        end 
+                    end 
+                end 
+            end 
+            
+            # game_sessions.each do | u |
+            #     next if users.include?(u.user_id)
+            #     users << u.user_id
+            #     scores << [User.where(id:u.user_id).first.present? ? User.find(u.user_id).name : "Deleted" ,u.score]
+            # end     
+        end 
+        scores[0..9].sort_by {|k,v| v}
+    end 
+
     def get_alltime_leaderboard(args={})
         game_id = args[:game_id]
         version = args[:version] ||= nil
