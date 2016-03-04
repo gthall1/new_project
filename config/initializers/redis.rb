3b608a8e9ac5ff75688b0226de1e0d4d5954b572
teylewr <tylerrules@gmail.com>
2016-03-04 15:00:25 -0500

    fixing password for reds

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,2 +1,6 @@
 uri = URI.parse(ENV["REDISCLOUD_URL"] || "redis://localhost:6379/")
-$redis = Redis.new(:host => uri.host, :port => uri.port)
+if !Rails.env.development?
+    $redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+else
+    $redis = Redis.new(:host => uri.host, :port => uri.port)
+end
