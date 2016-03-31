1167282c12411640bbfdb8d58550193788de725a
AlexMorgan <alexmorgan.am@gmail.com>
2016-03-30 23:55:05 -0400

    Added styling for us_only page

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160320151933) do
+ActiveRecord::Schema.define(version: 20160330152234) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -232,5 +232,13 @@ ActiveRecord::Schema.define(version: 20160320151933) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["remember_token"], name: "index_users_on_remember_token", using: :btree
+
+  create_table "waitlist_users", force: :cascade do |t|
+    t.string   "email"
+    t.integer  "arrival_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string   "country"
+  end
 
 end
